8dd878a3d85d2e5aa8965917431aa7583917e565
Ducky <lamson.vongoc@gmail.com>
2023-02-09 00:32:04 -0500

    add comments

--- include/LQR.hpp
+++ include/LQR.hpp
@@ -16,28 +16,75 @@ namespace Optimal_Controller
 class LQR
 {
   public:
+    /**
+     * @brief Construct a new LQR object
+     * 
+     * @param Q Weight on the systems state
+     * @param R Weight on control input
+     */
     LQR(const Eigen::MatrixXd& Q, const Eigen::MatrixXd& R);
+
+    /**
+     * @brief Destroy the LQR object
+     * 
+     */
     ~LQR() {};
 
+    /**
+     * @brief Disable copy constructor
+     * 
+     * @param iLQR 
+     */
+    LQR(LQR& iLQR) = delete;
+
+    /**
+     * @brief Disable assignment constructor
+     * 
+     * @param iLQR 
+     */
+    void operator=(LQR& iLQR) = delete;
+
+    /**
+     * @brief Get the command velocity
+     * 
+     * @param A System dynamics matrix
+     * @param B Input matrix
+     * @param E State error
+     * @return Eigen::MatrixXd 
+     */
     Eigen::MatrixXd getVelocity(const Eigen::MatrixXd& A, const Eigen::MatrixXd& B, const Eigen::MatrixXd& E);
 
   private:
     /**
+     * @brief Compute Hamiltionian
      * @brief An infinite-horizon LQR controller with penalties on the state feedback and input signal (Q and R, respectively)
      * @brief LQR gain is solved by searching for stable poles of the system through the Hamiltonian matrix
      * 
      * @param A System dynamics matrix
      * @param B Input matrix
-     * @param E State error
      * @return Eigen::MatrixXd LQR gain, K
      */
     void computeHamiltonian(const Eigen::MatrixXd& A, const Eigen::MatrixXd& B);
 
+    /**
+     * @brief Solve the Riccati Equation
+     * 
+     * @param A System dynamics matrix
+     * @param B Input matrix
+     * @param E State error 
+     */
     void computeRiccati(const Eigen::MatrixXd& A, const Eigen::MatrixXd& B, const Eigen::MatrixXd& E);
+    
+    /**
+     * @brief Compute velocity
+     * 
+     * @param A System dynamics matrix
+     * @param B Input matrix
+     * @param E State error 
+     */
     void computeVelocity(const Eigen::MatrixXd& A, const Eigen::MatrixXd& B, const Eigen::MatrixXd& E);
 
   private:
-    // Declare generic MAT for LQR computations
     Eigen::MatrixXd Q;            // Weight on the systems state
     Eigen::MatrixXd R;            // Weight on control input
     Eigen::MatrixXd I;            // Identity matrix
